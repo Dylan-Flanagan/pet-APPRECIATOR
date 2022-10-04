0d58c4b3aac79a303ec2b8b6aabb0d7cad987db0
Dylan <dylflanagan@gmail.com>
2022-10-03 22:22:52 -0700

    fixing errors

--- render-utils.js
+++ render-utils.js
@@ -8,8 +8,10 @@ export function renderPet(pet) {
     img.src = pet.image_url;
 
     const h2 = document.createElement('h2');
+    h2.textContent = pet.name;
 
     const p = document.createElement('p');
+    p.textContent = pet.bio;
 
     li.append(img, h2, p);
 
